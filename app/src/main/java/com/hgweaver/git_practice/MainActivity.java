8af211af6138d0d754b4002e9b1eb108d224da2f
samsung <runabird36@gmail.com>
2018-04-10 18:57:22 +0900

    add button

--- app/src/main/java/com/hgweaver/git_practice/MainActivity.java
+++ app/src/main/java/com/hgweaver/git_practice/MainActivity.java
@@ -2,14 +2,16 @@ package com.hgweaver.git_practice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        findViewById(R.id.button);
     }
 }
